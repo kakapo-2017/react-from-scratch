b481c966ce897d9740d8fe45721978df4b092039
mix irving <whimful@gmail.com>
2017-03-17 09:51:49 +1300

    dynamic template with action

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+var state = {name: 'Weka'}
 
-
+function changeName(ev) {
+  state.name = "kakapo"
+  render()
+}
 function App (props) {
+  var name = props.name
   return (
-    <div>HELLO WORLD</div>
-  )
+    <div>
+      <div>HELLO {name}</div>
+      <button onClick={changeName}>Change Name</button>
+    </div>
 
+  )
 }
 
 var target = document.getElementById('main')
 
 function render() {
-  var view = App()
+  var view = App(state)
   ReactDOM.render(view, target)
 }
 
